5e26193786cdaea49af6f772bba388d42fc0090f
Isfak-Shakil <isfakshakil@gmail.com>
2022-08-03 23:13:26 +0600

    login user and check user type

--- app/src/main/java/com/example/grocery/LoginActivity.java
+++ app/src/main/java/com/example/grocery/LoginActivity.java
@@ -1,19 +1,38 @@
 package com.example.grocery;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText emailEt,passwordEt;
     private TextView forgotTextView;
     private Button loginbtn,signUpLogBtn;
+    FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +46,11 @@ public class LoginActivity extends AppCompatActivity {
         loginbtn=findViewById(R.id.login_btn_Id);
         signUpLogBtn=findViewById(R.id.sign_up_Log_btn_Id);
 
+        firebaseAuth=FirebaseAuth.getInstance();
+        progressDialog=new ProgressDialog(this);
+        progressDialog.setTitle("Please wait..");
+        progressDialog.setCanceledOnTouchOutside(false);
+
         forgotTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,5 +63,96 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this,RegisterUserActivity.class));
             }
         });
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+    }
+    private String email,password;
+    private void loginUser() {
+        email=emailEt.getText().toString().trim();
+        password=passwordEt.getText().toString().trim();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(this,"Invalid Email address",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(this,"Empty password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressDialog.setMessage("Logging In..");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //loggin successful
+                        makeMeOnline();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    private void makeMeOnline() {
+        //after loggin ,make user online
+        progressDialog.setMessage("Checking user..");
+        HashMap<String,Object> hashMap=new HashMap<>();
+        hashMap.put("OnLine","true");
+        //update to db
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseAuth.getUid()).updateChildren(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //updated successfully
+                        checkUserType();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //update failed
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    private void checkUserType() {
+        //if user is seller ,start seller main activity
+        //if user is a buyer,start user main activity
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference("Users");
+        ref.orderByChild("uid").equalTo(firebaseAuth.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for (DataSnapshot ds:dataSnapshot.getChildren()){
+                            String accountType=""+ds.child("accountType").getValue();
+                            if (accountType.equals("Seller")){
+                                //user is  a seller
+                                progressDialog.dismiss();
+                                startActivity(new Intent(LoginActivity.this, MainSellerActivity.class));
+                                finish();
+                            }
+                            else{
+                                //user is  a buyer
+                                progressDialog.dismiss();
+                                startActivity(new Intent(LoginActivity.this, MainUserActivity.class));
+                                finish();
+
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
     }
 }
